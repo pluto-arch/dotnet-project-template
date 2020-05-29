a1b6a42575ba03f4a21ccde20fdcfff4b9a0f60d
zhangyulong <1976935046@qq.com>
2020-05-29 23:55:34 +0800

    add automapper

---
+++ template/content/src/Pluto.netcoreTemplate.API/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AutoMapper;
+
+
+namespace Pluto.netcoreTemplate.API
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            
+        }
+    }
+}
---
+++ template/content/src/Pluto.netcoreTemplate.Application/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AutoMapper;
+
+
+namespace Pluto.netcoreTemplate.Application
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            
+        }
+    }
+}
